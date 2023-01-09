e2f0360b8cebd4a641a4c1fb0f023662ce218a08
kevin1010607 <kevin.1010607@gmail.com>
2023-01-09 18:48:45 +0800

    update

--- js/my.js
+++ js/my.js
@@ -1,9 +1,23 @@
-// var triggerTabList = [].slice.call(document.querySelectorAll('#myTab a'))
-// triggerTabList.forEach(function (triggerEl) {
-//   var tabTrigger = new bootstrap.Tab(triggerEl)
+var all_tab = {};
+var current_tab = {};
 
-//   triggerEl.addEventListener('click', function (event) {
-//     event.preventDefault()
-//     tabTrigger.show()
-//   })
-// })
+window.onload = () => {
+    let tab = document.querySelectorAll('.tab');
+    for(let i of tab){
+        all_tab[i.id] = i;
+        current_tab[i.id] = 0;
+    }
+}
+
+openTab = (event, idx) => {
+    let name = event.currentTarget.parentNode.parentNode.id;
+    let links = all_tab[name].querySelector('.tab-all-links').children;
+    let contents = all_tab[name].querySelector('.tab-all-contents').children;
+
+    links[current_tab[name]].className = links[current_tab[name]].className.replace(' active', '');
+    contents[current_tab[name]].className = contents[current_tab[name]].className.replace(' active', '');
+    links[idx].className += ' active';
+    contents[idx].className += ' active'
+
+    current_tab[name] = idx;
+}
